7e52b38f243a074ad24010751b24e4cb0e3a916b
Frank Behrens <fbehrens@gmail.com>
2016-10-03 23:21:12 +0200

    SimpleRepl

--- extension.fs
+++ extension.fs
@@ -1,21 +1,48 @@
-module Demo 
+module SimpleRepl
 open Fable.Core
 open Fable.Import
 open Fable.Import.vscode
 open Ionide.VSCode.Helpers
 
-let activate (context : vscode.ExtensionContext) = 
-    printfn "Hello world"
-    
-    vscode.commands.registerCommand("extension.sayHello", fun _ ->
-        promise {
-            let ra = ["essen";"trinken"] |>ResizeArray 
-            let! r = vscode.window.showQuickPick( ra |> Case1)
-            printfn "selected %s" r
+let terminal = 
+  let t = window.createTerminal "my"
+  if JS.isDefined workspace.rootPath then
+    t.sendText((sprintf "cd %s" workspace.rootPath),true)
+  t.sendText("fsharpi",true)
+  t
 
-        } |> ignore
-        //r.``then``(fun s -> printfn "selected %s" s)
-        vscode.window.showInformationMessage "Hello my extension!" |> unbox )
-        
-    |> context.subscriptions.Add
-    
+let mutable _context : ExtensionContext option = None 
+
+let showQuickPick () = 
+  promise {
+    let ra = ["essen";"trinken"] |>ResizeArray 
+    let! r = vscode.window.showQuickPick( ra |> Case1)
+    printfn "selected %s" r
+   } 
+ 
+let information () =
+  promise {
+    let! a= vscode.window.showInformationMessage("Hello2", "a" ,"b")
+    printfn "chosen %A" a
+  }
+
+let pasteSelection () = 
+    let editor = window.activeTextEditor
+    let text = 
+      if editor.selection.isEmpty then
+        match _context with
+        | None -> ""
+        | Some c -> c.workspaceState.get "last"
+      else
+        let range = Range(editor.selection.anchor.line, editor.selection.anchor.character, editor.selection.active.line, editor.selection.active.character)
+        editor.document.getText range
+    _context |> Option.iter (fun c -> c.workspaceState.update("last",text) |> ignore )
+    terminal.sendText (text+";;"), true
+  
+let activate (context : vscode.ExtensionContext) = 
+  _context <- Some context
+  vscode.commands.registerCommand("extension.pasteSelection",pasteSelection |> unbox )
+  |> context.subscriptions.Add 
+
+let deactivate () =
+    ()
